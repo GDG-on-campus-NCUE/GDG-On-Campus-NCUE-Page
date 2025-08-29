ea308d8ec2d993bdbb8d4e2cd3e49f6e12445ed9
Grasonyang <grasonjas@gmail.com>
2025-08-29 11:59:39 +0000

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,10 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 export default {
   output: 'export',
-  basePath: isProd ? `/${repo}` : '',   // ⭐ 子路徑只在 production
+  basePath: 'gdg_web',   // ⭐ 子路徑只在 production
   images: { unoptimized: true },
   // 不要再設 assetPrefix，避免前綴被加兩次
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : ''
-  }
+//   env: {
+//     NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : ''
+//   }
 };
