36c1bf8e6295e967bf7fcb607531fa37c49de80c
Grasonyang <grasonjas@gmail.com>
2025-08-29 11:41:38 +0000

    update: deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	basePath: '/gdg_web',        // ⭐ 子路徑（一定要）
+  	assetPrefix: '/gdg_web/',    // ⭐ 靜態資源前綴（一定要）
+  	images: { unoptimized: true } // export 模式需要
 };
 
 export default nextConfig;
