2777ddc49be3d056f413160e7553346fd12c6ece
Tai Ming Chen <3526ming@gmail.com>
2025-09-03 02:05:12 +0800

    Update icon

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -15,16 +15,8 @@ import lineIcon from '@/images/icon/line.png';
 // Gmail 圖示，使用 SVG 繪製
 function GmailIcon({ className = '' }) {
     return (
-        <svg
-            className={className}
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            {/* 簡化版 Gmail 標誌 */}
-            <path
-                fill="currentColor"
-                d="M21.8 3H2.2A2.2 2.2 0 0 0 0 5.2v13.6A2.2 2.2 0 0 0 2.2 21h19.6a2.2 2.2 0 0 0 2.2-2.2V5.2A2.2 2.2 0 0 0 21.8 3zm0 2.2v.1L12 12.3 2.2 5.3v-.1h19.6zM21.8 18.8H2.2V6.8l9.8 7.5 9.8-7.5v12z"
-            />
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
+            <path fill="#4caf50" d="M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z"></path><path fill="#1e88e5" d="M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z"></path><polygon fill="#e53935" points="35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17"></polygon><path fill="#c62828" d="M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z"></path><path fill="#fbc02d" d="M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0 C43.076,8,45,9.924,45,12.298z"></path>
         </svg>
     );
 }
